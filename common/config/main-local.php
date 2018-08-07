0a9f942bc2605eecceac398ba1ccd6e600098cae
AlexMetalcore <alexandrov141089@gmail.com>
2018-08-07 22:12:02 +0300

    begin create admin/front part

---
+++ common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii',
+            'username' => 'yii',
+            'password' => '654apekru',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
+        ],
+    ],
+];
