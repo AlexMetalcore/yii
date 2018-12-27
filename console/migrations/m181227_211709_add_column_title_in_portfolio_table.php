aa59f4d7ac5531c9b88e6b79010c027486a18387
Alexandr Alexandrov <alexandrov141089@gmail.com>
2018-12-27 23:19:50 +0200

    add column title in portfolio table

---
+++ console/migrations/m181227_211709_add_column_title_in_portfolio_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181227_211709_add_column_title_in_portfolio_table
+ */
+class m181227_211709_add_column_title_in_portfolio_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('portfolio', 'title', 'VARCHAR(255) AFTER `id` ');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('portfolio', 'title');
+    }
+}
