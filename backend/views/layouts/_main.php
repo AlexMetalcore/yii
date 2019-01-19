7b387c5f143f0405bb88eb56e86704f51f0d44bc
Alexandr Alexandrov <alexandrov141089@gmail.com>
2019-01-19 23:19:46 +0200

    add admin panel, setting this panel and rules for user,change tooltip for like

---
+++ backend/views/layouts/_main.php
@@ -0,0 +1,92 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use common\widgets\Alert;
+use yii\helpers\Url;
+use backend\models\User;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?= $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Url::to(['/../favicon.png'])]); ?>
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'Админ панель' .Html::a('( Перейти на сайт )' , ['/../'] , ['class' => 'go-site']),
+        'brandUrl' => '/admin/',
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+            'visible' => !Yii::$app->user->isGuest,
+        ],
+    ]);
+    $auth = Yii::$app->authManager;
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Авторизация', 'url' => ['/site/login']];
+    } else {
+        if ($auth->getRole('admin')) {
+            $menuItems[] = ['label' => 'Статьи', 'url' => ['/post/index']];
+            $menuItems[] = ['label' => 'Категории', 'url' => ['/category/index']];
+            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Пользователи', 'url' => ['/user/index']] : '';
+            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Портфолио работ', 'url' => ['/portfolio/index']] : '';
+            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Настройки', 'options'=>['class'=>'dropdown'] ,'items' => [
+                ['label' => 'Параметры настроек', 'url' => ['settings/index']],
+                ['label' => 'Кеш и работа с картинками', 'url' => ['settings/cache-data-img']]
+            ]] : '';
+            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Медиафайлы', 'url' => ['/media/index']] : '';
+            $menuItems[] = '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton('Выход (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>';
+        }
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'homeLink' => ['label' => 'Главная', 'url' => '/admin'],
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?php if( Yii::$app->session->hasFlash('success') ): ?>
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <?php echo Yii::$app->session->getFlash('success'); ?>
+            </div>
+        <?php endif;?>
+        <?= $content ?>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,92 +1,65 @@
 <?php
+use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use backend\assets\AppAsset;
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use common\widgets\Alert;
-use yii\helpers\Url;
-use backend\models\User;
 
-AppAsset::register($this);
-?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+if (Yii::$app->controller->action->id === 'login') { 
+/**
+ * Do not use this code in your template. Remove it. 
+ * Instead, use the code  $this->layout = '//main-login'; in your controller.
+ */
+    echo $this->render(
+        'main-login',
+        ['content' => $content]
+    );
+} else {
 
-<div class="wrap">
-    <?= $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Url::to(['/../favicon.png'])]); ?>
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Админ панель' .Html::a('( Перейти на сайт )' , ['/../'] , ['class' => 'go-site']),
-        'brandUrl' => '/admin/',
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-            'visible' => !Yii::$app->user->isGuest,
-        ],
-    ]);
-    $auth = Yii::$app->authManager;
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Авторизация', 'url' => ['/site/login']];
+    if (class_exists('backend\assets\AppAsset')) {
+        backend\assets\AppAsset::register($this);
     } else {
-        if ($auth->getRole('admin')) {
-            $menuItems[] = ['label' => 'Статьи', 'url' => ['/post/index']];
-            $menuItems[] = ['label' => 'Категории', 'url' => ['/category/index']];
-            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Пользователи', 'url' => ['/user/index']] : '';
-            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Портфолио работ', 'url' => ['/portfolio/index']] : '';
-            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Настройки', 'options'=>['class'=>'dropdown'] ,'items' => [
-                ['label' => 'Параметры настроек', 'url' => ['settings/index']],
-                ['label' => 'Кеш и работа с картинками', 'url' => ['settings/cache-data-img']]
-            ]] : '';
-            $menuItems[] = User::findIdentity(\Yii::$app->user->identity->getId())->status != User::ROLE_MODERATOR ? ['label' => 'Медиафайлы', 'url' => ['/media/index']] : '';
-            $menuItems[] = '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton('Выход (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>';
-        }
+        app\assets\AppAsset::register($this);
     }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
+
+    dmstr\web\AdminLteAsset::register($this);
+
+    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
     ?>
+    <?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+    <?php $this->beginBody() ?>
+    <div class="wrapper">
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'homeLink' => ['label' => 'Главная', 'url' => '/admin'],
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?php if( Yii::$app->session->hasFlash('success') ): ?>
-            <div class="alert alert-success alert-dismissible" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span></button>
-                <?php echo Yii::$app->session->getFlash('success'); ?>
-            </div>
-        <?php endif;?>
-        <?= $content ?>
+        <?= $this->render(
+            'header.php',
+            ['directoryAsset' => $directoryAsset]
+        ) ?>
+
+        <?= $this->render(
+            'left.php',
+            ['directoryAsset' => $directoryAsset]
+        )
+        ?>
+
+        <?= $this->render(
+            'content.php',
+            ['content' => $content, 'directoryAsset' => $directoryAsset]
+        ) ?>
+
     </div>
-</div>
 
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage() ?>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+    <?php $this->endPage() ?>
+<?php } ?>
